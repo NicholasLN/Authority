feea92c7bacbc5789553ca0b6a0d4faac1631425
PhilScott4VT <44556938+PhilScott4VT@users.noreply.github.com>
2021-01-07 13:53:13 -0500

    FavIcon

--- authority/php/essentials/htmlFunctions.php
+++ authority/php/essentials/htmlFunctions.php
@@ -36,6 +36,9 @@ ALERT;
     function echoStyles(){
         echo '<link href="css/main.css?id=84" rel="stylesheet">';
     }
+    function echoFavIcon(){
+        echo '<link rel="icon" type="image/png" href="images/AuthorityLogoSMALL.png">';
+    }
     function echoBootstrap(){
         // STYLE
         echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" 
@@ -53,6 +56,7 @@ ALERT;
         echoJQuery();
         echoBootstrap();
         echoFA();
+        echoFavIcon();
         echoStyles();
 
     }
