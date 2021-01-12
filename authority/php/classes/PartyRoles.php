03d6771e43599bf12a80206c258262ed60ed362d
PhilScott4VT <44556938+PhilScott4VT@users.noreply.github.com>
2021-01-12 14:42:45 -0500

    Two new functions. 1) echoRoleCard() which is used for the party roles view, and 2) getRoleTitle() for users to grab their role within the party

--- authority/php/classes/PartyRoles.php
+++ authority/php/classes/PartyRoles.php
@@ -23,10 +23,45 @@ class PartyRoles
             }
         }
     }
+    public function echoRoleCard(){
+        foreach($this->partyRoleJson as $roleName=>$roleDetails){
+            if(!array_key_exists("leader",$roleDetails['perms'])) {
+                    $occupant = new User($roleDetails['occupant']);
+                    $occupantPic = $occupant->pictureArray()['picture'];
+                    $occupantName = $occupant->pictureArray()['name'];
+                    $occupantID = $occupant->pictureArray()['id'];
+
+                    echo "
+                <div class='col-sm-3' style='margin-top: 8px'>
+                    <span>$roleName</span>
+                    <br/>
+                    <a href='politician.php?id=$occupantID'>
+                        <img style='max-width:80px;height:75px;border:5px ridge darkgrey' src='$occupantPic'/>
+                        <p>$occupantName</p>
+                    </a>
+                </div>    
+                ";
+            }
+
+        }
+    }
     public function partyLeaderTitle(){
         return $this->partyLeaderArray()['title'];
     }
+    public function getUserTitle(int $userID){
+        $hasRole = 0;
+        foreach($this->partyRoleJson as $roleName=>$roleDetails){
+            if($roleDetails['occupant'] == $userID){
+                $hasRole = 1;
+                return $roleName;
+            }
+        }
+        if($hasRole == 0){
+            return "Member";
 
+        }
+
+    }
 
 
 }
