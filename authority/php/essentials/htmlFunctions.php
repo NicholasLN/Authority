dcdba26d39cb05fa23932657baf8f76b13916349
PhilScott4VT <44556938+PhilScott4VT@users.noreply.github.com>
2021-01-12 22:24:14 -0500

    Update to allow alerts after redirecting. Uses $_GET functionality.

--- authority/php/essentials/htmlFunctions.php
+++ authority/php/essentials/htmlFunctions.php
@@ -69,16 +69,19 @@ ALERT;
         <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>
         <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>
         ";
-        if($_SESSION['loggedIn']){
-        echo "
-        <div class='footerInformation'>     
-            <p>
-                <b>AUTHORITY:</b> ".$loggedInRow['authority']." <b>||</b>
-                <b>CF: $</b><span class='greenFont'>".number_format($loggedInRow['campaignFinance'])."</span>
-            </p> 
-        </div>
-        ";
-
+        if($_SESSION['loggedIn']) {
+            echo "
+            <div class='footerInformation'>     
+                <p>
+                    <b>AUTHORITY:</b> " . $loggedInRow['authority'] . " <b>||</b>
+                    <b>CF: $</b><span class='greenFont'>" . number_format($loggedInRow['campaignFinance']) . "</span>
+                </p> 
+            </div>
+            ";
+        }
+        // Alert Redirect Handling
+        if (isset($_GET['alertHeader']) && isset($_GET['alertMsg'])) {
+            alert($_GET['alertHeader'], $_GET['alertMsg']);
 
         }
     }
