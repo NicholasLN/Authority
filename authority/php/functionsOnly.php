716d5847e94277d92c0baea59d3bd33ac909b83e
PhilScott4VT <44556938+PhilScott4VT@users.noreply.github.com>
2021-01-16 21:38:16 -0500

    lots of fucking shit

---
+++ authority/php/functionsOnly.php
@@ -0,0 +1,13 @@
+<?php
+$onlineThreshold = time() - 259200;
+
+include 'functions/queryFunctions.php';
+include 'classes/classWrapper.php';
+
+include 'essentials/registerLogin.php';
+include 'essentials/htmlFunctions.php';
+include 'essentials/navBar.php';
+
+include 'functions/positionFunctions.php';
+include 'functions/usefulFunctions.php';
+include 'functions/discordMeta.php';
