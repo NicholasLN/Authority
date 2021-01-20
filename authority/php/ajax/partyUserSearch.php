ce6d7b46e29bad55ed8eab5ca4f4ed1ff1d444b4
PhilScott4VT <44556938+PhilScott4VT@users.noreply.github.com>
2021-01-20 17:13:32 -0500

    made it so ppl already with a role dont show up

--- authority/php/ajax/partyUserSearch.php
+++ authority/php/ajax/partyUserSearch.php
@@ -1,13 +1,14 @@
 <?php
 include './ajaxWrapper.php';
 
-$searchLimit = 15;
+$searchLimit = 10;
 mysqli_set_charset($db,"utf8");
 $party = $_POST['partyID'];
+$pClass = new Party($party);
 $loggedInID = $_POST['loggedInID'];
 
 if(!isset($_POST['searchTerm'])){
-    $stmt = $db->prepare("SELECT id, politicianName as `text` FROM users WHERE party = ? AND id != ? AND lastOnline > ? ORDER BY id LIMIT ?");
+    $stmt = $db->prepare("SELECT id, politicianName as `text` FROM users WHERE party = ? AND id != ? AND lastOnline > ? ORDER BY partyInfluence LIMIT ?");
     $stmt->bind_param("iiii",$party,$loggedInID,$onlineThreshold,$searchLimit);
     $stmt->execute();
     $userList = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
@@ -16,13 +17,22 @@ else{
     $search = $_POST['searchTerm']."%";// Search text
 
     // Fetch records
-    $stmt = $db->prepare("SELECT id, politicianName as `text` FROM users WHERE politicianName like ? AND party = ? AND id != ? AND lastOnline > ? ORDER BY id LIMIT ?");
+    $stmt = $db->prepare("SELECT id, politicianName as `text` FROM users WHERE politicianName like ? AND party = ? AND id != ? AND lastOnline > ? ORDER BY partyInfluence LIMIT ?");
     $stmt->bind_param("siiii",$search, $party, $loggedInID,$onlineThreshold,$searchLimit);
     $stmt->execute();
 
     $userList = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 }
+$userList2 = array();
 
-$json = json_encode($userList);
+foreach($userList as &$user){
+    if($pClass->partyRoles->getUserTitle($user['id']) == "Member"){
+        $arr = array("id"=>$user['id'],"text"=>$user['text']);
+        array_push($userList2,$arr);
+    }
+}
+//var_dump($userList);
+
+$json = json_encode($userList2);
 echo $json;
 exit();
