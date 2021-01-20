1920e3f5fd67b2dab16bc4afb78267132144ccca
PhilScott4VT <44556938+PhilScott4VT@users.noreply.github.com>
2021-01-20 17:14:09 -0500

    Party Roles! Can now create positions.

--- authority/party.php
+++ authority/party.php
@@ -41,7 +41,7 @@ if (isset($_GET['id'])) {
         <div class="gameContainer">
             <div class="row">
                 <div class="col-sm"></div>
-                <div class="col-sm-8">
+                <div class="col-sm-9">
                     <br/>
                     <h2><? echo $party->getPartyName() ?> </h2>
                     <img style="max-width:150px;max-height:150px;" src="<? echo $party->getPartyLogo() ?>"
@@ -49,7 +49,6 @@ if (isset($_GET['id'])) {
 
 
                     <? // Join/Leave Party Button and PHP //
-                    // TODO: Confirmation window for leaving or defecting.
                     if ($_SESSION['loggedIn'] == True) {
                         // if they are within the same nation...
                         if ($loggedInRow['nation'] == $party->partyRow['nation']) {
@@ -201,9 +200,7 @@ if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == True) {
 
         }
     }
-
     //TODO: Turn this into an Ajax form request instead of a PHP Submit.
-
     if (isset($_POST['voteFor'])) {
         if (isset($_POST['voteForID']) && $_POST['voteForID'] != 0) {
             $votingFor = new User(numFilter($_POST['voteForID']));
@@ -213,6 +210,104 @@ if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == True) {
             } else {
                 alert("Error", "They are not in your party.");
             }
+        }
+    }
+    if (isset($_POST['createPosSubmit'])){
+        //var_dump($_POST);
+        if(isset($_POST['roleCheck']) && sizeof($_POST['roleCheck']) <= 3){
+            $illegitimate = 0;
+            foreach ($_POST['roleCheck'] as $key=>$value){
+                switch ($value) {
+                    case "delayVoteCheck":
+                    case "feeChangeCheck":
+                    case "purgeMemberCheck":
+                    case "sendFundsCheck":
+                    case "fundingReqCheck":
+                    case "partyAnnounceCheck":
+                        break;
+                    default:
+                        $illegitimate = 1;
+                        break;
+                }
+            }
+            if($illegitimate==0){
+                $partyRoleUser = new User(numFilter($_POST['partySearch']));
+                if($partyRoleUser->isUser){
+                    if($partyRoleUser->getVariable("party")==$partyID){
+                        if($party->partyRoles->getRoleCount() < 3) {
+                            if($party->partyRoles->getUserTitle($partyRoleUser->userID) == "Member") {
+                                $party->partyRoles->createNewRole($_POST['roleName'], $_POST['partySearch'], $_POST['roleCheck']);
+                            }
+                            else{
+                                alert("Error!","They already have a role.");
+                            }
+                        }
+                        else{
+                            alert("Error!","You currently have too many roles. This limit will soon be related to national party influence in coming updates.");
+                        }
+                    }
+                    else{
+                        alert("Error!","Not within ya party. Do not HTML Spoof!");
+                    }
+                }
+                else{
+                    alert("Error!","Not a user. Do not HTML Spoof next time.");
+                }
+            }
+            else{
+                alert("Caught ya!","Thought you could get away with HTML Spoofing? Well, guess what, I just told a mod!");
+            }
+        }
+    }
+    if (isset($_POST['newPartyDescSubmit'])){
+        if(!isset($_POST['newPartyDesc'])){
+            $newDesc = "";
+        }
+        else{
+            $newDesc= $_POST['newPartyDesc'];
+        }
+        $newDesc = strip_tags($newDesc);
+        $newDesc = preg_replace('/(\r\n|\n|\r){3,}/', "$1$1$1", $newDesc);
+        $party->updateVariable("partyBio",$newDesc);
+        redirect("party.php?id=$partyID","Success!","Changed Party Bio");
+
+    }
+    if(isset($_POST['newPartyPicSubmit'])){
+        $directory = "images/partyPics";
+        $file = $directory . basename($_FILES['newPartyPicture']["name"]);
+        $imageFileType = pathinfo($file, PATHINFO_EXTENSION);
+        $check = getimagesize($_FILES['newPartyPicture']["tmp_name"]);
+        // is image
+        if (($check !== false)) {
+            if ($_FILES['newPartyPicture']['size'] < 500000) {
+                $name = $party->getVariable("name");
+                $pic = $party->getVariable("partyPic");
+                if($party->getVariable("partyPic") != "images/partyPics/independent.png") {
+                    $pic = substr($pic, 0, strpos($pic, "?ver="));
+                    unlink($pic);
+                }
+                try {
+                    $rand = random_int(PHP_INT_MIN, PHP_INT_MAX);
+                } catch (Exception $e) {
+                    $rand = 92393;
+                }
+                $moved = move_uploaded_file($_FILES["newPartyPicture"]["tmp_name"], "$directory/$name.$imageFileType");
+
+                $fileString = "images/partyPics/$name.$imageFileType?ver=$rand";
+                $party->updateVariable("partyPic", $fileString);
+
+                if ($moved) {
+                    redirect("party.php?id=$partyID");
+                } else {
+                    echo $_FILES['file']['error'];
+                }
+            } else {
+                $kb = round($_FILES['newPartyPicture']['size'] / 1000);
+                $mb = $kb / 1000;
+                alert("Error!", "File is too large. Can not be over 500kb (0.5mb)<br/>Your file is $kb kb ($mb mb)");
+            }
+        } else {
+            alert("h", print_r($check));
         }
     }
 }
