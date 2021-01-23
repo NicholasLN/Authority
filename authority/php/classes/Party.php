433081bdae62d3a2f01228a698816b84cf3c7844
PhilScott4VT <44556938+PhilScott4VT@users.noreply.github.com>
2021-01-23 02:12:31 -0500

    Rearranging methods

--- authority/php/classes/Party.php
+++ authority/php/classes/Party.php
@@ -6,22 +6,13 @@ class Party
     public $partyRoles;
     public $partyRow;
 
-    public function getPartyLeader(): ?User
-    {;
-        $leader = $this->partyRoles->partyLeaderArray();
-        if($leader['occupant'] != 0){
-            return new User($leader['occupant']);
-        }
-        else{
-            return new User(0);
-        }
-    }
     public function __construct($partyID)
     {
         $this->partyID = $partyID;
         $this->partyRow = $this->getPartyRowConstructor($partyID);
-        $this->partyRoles = new PartyRoles($this->partyRow['partyRoles'],$partyID);
+        $this->partyRoles = new PartyRoles($this->partyRow['partyRoles'], $partyID);
     }
+
     public function getPartyRowConstructor($partyID): ?array
     {
         global $db;
@@ -30,26 +21,108 @@ class Party
         $stmt->execute();
         return $stmt->get_result()->fetch_assoc();
     }
-    public function updatePartyRow(){
+
+    public function updatePartyRow()
+    {
         global $db;
         $id = $this->partyID;
         $this->partyRow = $this->getPartyRowConstructor($this->partyID);
     }
+
+    private function type($var): string
+    {
+        if ($var == "string") {
+            return "si";
+        }
+        if ($var == "integer") {
+            return "ii";
+        }
+        if ($var == "double") {
+            return "di";
+        }
+    }
+
+    public function updateVariable($variable, $to)
+    {
+        global $db;
+        $st = $db->prepare("UPDATE parties SET " . $variable . " = ? WHERE id=?");
+        $st->bind_param($this->type(gettype($to)), $to, $this->partyID);
+        $st->execute();
+    }
+
+    public function pictureArray($profile = false): array
+    {
+        $partyID = $this->partyID;
+        $array = array("picture" => 0, "name" => 0, "id" => 0);
+        if ($partyID != 0) {
+            $array['picture'] = $this->getVariable("partyPic");
+            $array['name'] = $this->getVariable("name");
+            $array['id'] = $this->getVariable("id");
+        } else {
+            if ($profile == true) {
+                $array['picture'] = 'images/partyPics/independent.png?ver=1';
+            } else {
+                $array['picture'] = "images/partyPics/default.png";
+            }
+            $array['name'] = "Independent";
+            $array['id'] = 0;
+        }
+        return $array;
+    }
+
+    public function getTotalPartyInfluence()
+    {
+        global $db;
+        global $onlineThreshold;
+        $total = $db->query("SELECT SUM(partyInfluence) as partyInfluenceSum FROM users WHERE party = " . $this->partyID . " AND lastOnline>$onlineThreshold");
+        $total = $total->fetch_assoc();
+        return round($total['partyInfluenceSum'], 2);
+    }
+
+    public function getPartyLeader(): ?User
+    {
+        $leader = $this->partyRoles->partyLeaderArray();
+        if ($leader['occupant'] != 0) {
+            return new User($leader['occupant']);
+        } else {
+            return new User(0);
+        }
+    }
+
     public function getPartyName()
     {
         return $this->partyRow['name'];
     }
-    public function getPartyBio(){
+
+    public function getPartyBio()
+    {
         return $this->partyRow['partyBio'];
     }
+
     public function getPartyLogo()
     {
         global $db;
-        return $this->partyRow['partyPic'];
+        if($this->partyRow['partyPic'] == ""){
+            return "images/partyPics/independent.png";
+        }
+        else {
+            return $this->partyRow['partyPic'];
+        }
     }
-    public function getPartyDiscordCode(){
+
+    public function getFundingRequestsArray() : array{
+        global $db;
+        $stmt = $db->prepare("SELECT * FROM fundRequests WHERE party = ? AND fulfilled = 0");
+        $stmt->bind_param("i",$this->partyID);
+        $stmt->execute();
+        return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getPartyDiscordCode()
+    {
         return $this->partyRow['discord'];
     }
+
     public function getPartyMembers()
     {
         global $db;
@@ -63,61 +136,14 @@ class Party
         $stmt->execute();
         return $stmt->get_result()->num_rows;
     }
+
     public function getVariable($variable)
     {
-        if(array_key_exists($variable,$this->partyRow)){
+        if (array_key_exists($variable, $this->partyRow)) {
             return $this->partyRow[$variable];
-        }
-        else{
+        } else {
             return null;
         }
-    }
-    private function type($var): string
-    {
-        if ($var == "string") {
-            return "si";
-        }
-        if ($var == "integer") {
-            return "ii";
-        }
-        if ($var == "double") {
-            return "di";
-        }
-    }
-    public function updateVariable($variable, $to)
-    {
-        global $db;
-        $st = $db->prepare("UPDATE parties SET " . $variable . " = ? WHERE id=?");
-        $st->bind_param($this->type(gettype($to)), $to, $this->partyID);
-        $st->execute();
-    }
-    public function getTotalPartyInfluence(){
-        global $db;
-        global $onlineThreshold;
-        $total = $db->query("SELECT SUM(partyInfluence) as partyInfluenceSum FROM users WHERE party = ".$this->partyID." AND lastOnline>$onlineThreshold");
-        $total = $total->fetch_assoc();
-        return round($total['partyInfluenceSum'],2);
-    }
-    public function pictureArray($profile = false): array
-    {
-        $partyID = $this->partyID;
-        $array = array("picture"=>0, "name"=>0,"id"=>0);
-        if($partyID != 0){
-            $array['picture'] = $this->getVariable("partyPic");
-            $array['name'] = $this->getVariable("name");
-            $array['id'] = $this->getVariable("id");
-        }
-        else{
-            if($profile == true){
-                $array['picture'] = 'images/partyPics/independent.png?ver=1';
-            }
-            else {
-                $array['picture'] = "images/partyPics/default.png";
-            }
-            $array['name'] = "Independent";
-            $array['id'] = 0;
-        }
-        return $array;
     }
 }
 
