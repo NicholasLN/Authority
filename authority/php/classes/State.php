9f6bc15b85d4744606a6de734bcd0063fd803482
Tadeo Diaz <td12233a@gmail.com>
2021-03-07 02:24:34 -0600

    yay we did lots!!!! (in regards to states) 😺
    added cool function getStatePlayers(), which gets state players lol
    added button to demographics.php lol
    added getPartyFromId() to usefulFunctions.php
    setup basic version of state.php!!!! 🐈🐈🐈🐈
    also committed changes to positionFunctions.php that we didnt push 🙀
    
    - skyler helped 🐱🐱

--- authority/php/classes/State.php
+++ authority/php/classes/State.php
@@ -19,16 +19,29 @@ class State
         if ($result -> num_rows == 1) {
             $this -> doesItExist = true;
             $this -> stateAbbr = $abbreviation;
+            $this -> stateInfoArray = $result -> fetch_array(MYSQLI_ASSOC);
         } else {
             $this -> doesItExist = false;
         }
     }
 
-    public function getDemographics()
+    public function getDemographics(): array
     {
         global $db;
 
         $stmt = $db -> prepare("SELECT * FROM demographics WHERE State = ?");
+        $stmt -> bind_param("s", $this -> stateAbbr);
+        $stmt -> execute();
+
+        $result = $stmt -> get_result();
+        return $result -> fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getStatePlayers(): array
+    {
+        global $db;
+
+        $stmt = $db -> prepare("SELECT * FROM users WHERE state = ?");
         $stmt -> bind_param("s", $this -> stateAbbr);
         $stmt -> execute();
 
