7d7709817b62ec29ac0f54742caf7c1129ecbecd
philscott <philscott4usa@gmail.com>
2021-03-07 04:34:58 -0500

    Refurbished State a little bit. It'll now programatically fill in the state flag. Also, modified the politician table.

--- authority/php/classes/State.php
+++ authority/php/classes/State.php
@@ -16,36 +16,57 @@ class State
 
         $result = $stmt -> get_result();
 
-        if ($result -> num_rows == 1) {
-            $this -> doesItExist = true;
-            $this -> stateAbbr = $abbreviation;
-            $this -> stateInfoArray = $result -> fetch_array(MYSQLI_ASSOC);
+        if ($result->num_rows == 1) {
+            $this->doesItExist = true;
+            $this->stateAbbr = $abbreviation;
+            $this->stateInfoArray = $result->fetch_array(MYSQLI_ASSOC);
         } else {
-            $this -> doesItExist = false;
+            $this->doesItExist = false;
         }
+    }
+
+    public function getCountry(): string
+    {
+        global $db;
+        $stmt = $db->prepare("SELECT country FROM states WHERE abbreviation = ?");
+        $stmt->bind_param("s", $this->stateAbbr);
+
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_array()['country'];
+
     }
 
     public function getDemographics(): array
     {
         global $db;
 
-        $stmt = $db -> prepare("SELECT * FROM demographics WHERE State = ?");
-        $stmt -> bind_param("s", $this -> stateAbbr);
-        $stmt -> execute();
+        $stmt = $db->prepare("SELECT * FROM demographics WHERE State = ?");
+        $stmt->bind_param("s", $this->stateAbbr);
+        $stmt->execute();
 
-        $result = $stmt -> get_result();
-        return $result -> fetch_all(MYSQLI_ASSOC);
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getStatePlayers(): array
+    public function getStatePlayers(bool $active = True, bool $sortByInfluence = True): array
     {
         global $db;
+        // for hiding inactives
+        global $onlineThreshold;
 
-        $stmt = $db -> prepare("SELECT * FROM users WHERE state = ?");
-        $stmt -> bind_param("s", $this -> stateAbbr);
-        $stmt -> execute();
+        $stmt = $db->prepare("SELECT * FROM users WHERE state = ? AND lastOnline > ?");
+        $stmt->bind_param("si", $this->stateAbbr, $onlineThreshold);
+        $stmt->execute();
 
-        $result = $stmt -> get_result();
-        return $result -> fetch_all(MYSQLI_ASSOC);
+        $result = $stmt->get_result();
+        $array = $result->fetch_all(MYSQLI_ASSOC);
+        if ($sortByInfluence) {
+            usort($array, function ($item1, $item2) {
+                return $item2['hsi'] <=> $item1['hsi'];
+            });
+        }
+        return $array;
     }
 }
--- authority/state.php
+++ authority/state.php
@@ -2,10 +2,12 @@
 $state = null;
 $stateArray = null;
 if (isset($_GET['state'])) {
-    $state = new State($_GET['state']);
 
-    if ($state -> doesItExist) {
-        $stateArray = $state -> stateInfoArray;
+    $state = new State($_GET['state']);
+    $country = $state->getCountry();
+
+    if ($state->doesItExist) {
+        $stateArray = $state->stateInfoArray;
     } else {
         invalidPage("Invalid State!", "Invalid State asshole.");
     }
@@ -19,33 +21,6 @@ if (isset($_GET['state'])) {
     <meta charset="utf-8"/>
     <title>Authority</title>
     <? echoHeader(); ?>
-    <style>
-        tr {
-            width: 100%;
-            display: inline-table;
-            table-layout: fixed;
-        }
-
-        table thead {
-            height: 300px;
-            display: -moz-groupbox;
-            table-layout: fixed;
-        }
-
-        tbody {
-            flex: 1 1 auto;
-            display: block;
-            height: 250px;
-            overflow-y: scroll;
-        }
-        table thead tbody td tr {
-            border-style: none !important;
-        }
-
-         td {
-            padding: 10px !important;
-        }
-    </style>
 </head>
 <? echoNavBar() ?>
 <body>
@@ -56,42 +31,45 @@ if (isset($_GET['state'])) {
             <div class="col-sm-10">
                 <h4 style="padding-top: 16px"><i>The State of</i></h4>
                 <h1><b><?= $stateArray['name'] ?></b></h1>
-                <img width="200" src="/images/flags/United%20States/<?= $stateArray['flag'] ?>" alt="State flag"/>
+                <img width="200" src="images/flags/<?= $country ?>/<?= $stateArray['flag'] ?>" alt="State flag"/>
                 <hr>
-                <a href="demographics.php?state=<?= $state -> stateAbbr?>" class="btn btn-primary ">Demographics</a>
-                <div class="col-sm-4">
-                <table class="table table-striped table-bordered table-sm">
+                <a href="demographics.php?state=<?= $state->stateAbbr ?>" class="btn btn-primary ">Demographics</a>
+
+                <table class="table table-striped">
                     <thead>
-                        <tr>
-                            <th class="th-sm" style="text-align: left;">Name</th>
-                            <th class="th-sm" style="text-align: left;">Party</th>
-                            <th class="th-sm" style="text-align: left;">State Influence</th>
-                        </tr>
+                    <tr>
+                        <th class="th-sm" style="text-align: left;">Name</th>
+                        <th class="th-sm" style="text-align: left;">Party</th>
+                        <th class="th-sm" style="text-align: left;">State Influence</th>
+                    </tr>
                     </thead>
-                        <? $playerList = $state -> getStatePlayers(); ?>
+                    <? $playerList = $state->getStatePlayers(); ?>
                     <tbody>
                     <?
-                    foreach ($playerList as $player)
-                    {?>
-                    <tr>
-                        <td style="border-style: none;">
-                            <a href="politician.php?id=<?=$player['id']?>"><?=$player['politicianName']?></a>
-                        </td>
+                    foreach ($playerList as $player) {
+                        ?>
+                        <tr>
+                            <td style="border-style: none;">
+                                <a href="politician.php?id=<?= $player['id'] ?>"><?= $player['politicianName'] ?></a>
+                            </td>
 
-                        <td style="text-align: left; border-style: none;">
-                            <? $party = getPartyFromId($player['party']) ?>
-                            <a href="party.php?id=<?=$party['id']?>"><?=$party['name']?></a>
-                        </td>
+                            <td style="text-align: left; border-style: none;">
+                                <?
+                                $party = new Party($player['party']);
+                                $partyInformation = $party->pictureArray(true);
+                                ?>
+                                <a href="party.php?id=<?= $partyInformation['id'] ?>"><?= $partyInformation['name'] ?></a>
+                            </td>
 
-                        <td style="text-align: left; border-style: none;">
-                            <?= $player['hsi'] ?>%
-                        </td>
-                    </tr>
-                    <?}?>
+                            <td style="text-align: left; border-style: none;">
+                                <?= $player['hsi'] ?>%
+                            </td>
+                        </tr>
+                    <? } ?>
                     </tbody>
                 </table>
-                </div>
             </div>
+
             <div class="col-sm"></div>
         </div>
     </div>
