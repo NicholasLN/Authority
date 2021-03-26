0ea08c5fc42e514952f5309adba40ef5193ce5ef
PhilScott <philscott4usa@gmail.com>
2021-03-26 00:42:51 -0400

    Approval stuff.

--- authority/php/classes/DemographicSupport.php
+++ authority/php/classes/DemographicSupport.php
@@ -1,12 +1,38 @@
 <?
-class DemographicApproval
+class DemographicSupport
 {
     public static function getPositionDifferenceApproval($positionValue, $demographicValue){
         $difference = abs($positionValue-$demographicValue); 
         //   limit (100)
-        $y = 100-($difference**2)/0.3;
+        $y = round(75+log($difference+1, 0.975),2);
+        if($y>=100){ $y = 75;}
+        if($y<0){ $y = 0;}
 
-        return $y;
+        return max($y,0);
+    }
+
+    // This should be extended later on to incorporate several other static functions involving party and other variables.
+    public static function getTotalApproval($demographicPositions, $loggedInUser): float{
+        global $memcached;
+        $userID = $loggedInUser->userID;
+
+
+        $demographicSocial = $demographicPositions['social'];
+        $demographicEco = $demographicPositions['economic'];
+
+        $userSocial = $loggedInUser->socPos;
+        $userEconomic = $loggedInUser->ecoPos;
+
+        $positionApproval = self::getPositionDifferenceApproval($userSocial, $demographicSocial)
+                            +
+                            self::getPositionDifferenceApproval($userEconomic, $demographicEco)
+                            / 2;
+        if($positionApproval>=100){
+            $positionApproval = 75;
+        }
+
+        return round($positionApproval,2);
+
     }
 
 }
